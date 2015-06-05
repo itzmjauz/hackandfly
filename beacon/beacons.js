fd2a8ed9687d37957a56bb66d83b49b28e837764
itzmjauz <itzmjauz@gmail.com>
2015-06-06 00:00:28 +0200

    working results

--- beacon/beacons.js
+++ beacon/beacons.js
@@ -3,11 +3,25 @@ var request = require('request')
   , app_key = "6398e33d6453f3dfcccb82ebb44ca473"
   ,    link = "https://cube.api.aero/atibeacon/beacons/1?airportCode=AMS&app_id="+app_id+"&app_key="+app_key+"&preservePendingNewBeacons=true"
 
-request 
-    .get(link)
-    .on('response', function(response){
-        console.log(response.statusCode)
-    })
+
+var options = {
+     url     : link
+  ,  method  : 'GET'
+  ,  headers : {
+    'User-Agent': 'request'
+  }
+}
+
+
+function callback(error, response, body){
+  if (!error && response.statusCode == 200) {
+    jsonData = JSON.parse(body)
+    console.log(jsonData)
+  }
+}
+
+
+request(options, callback)
 
 
 
